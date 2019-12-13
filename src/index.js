5c6cc7b964d71565ffc9dfacf98853a27402667d
Moskvichev Vladislav <moskvichev.exceedteam@gmail.com>
2019-12-13 12:27:19 +0300

    work is done

--- src/index.js
+++ src/index.js
@@ -2,10 +2,4 @@ import { messages } from './messages';
 // Подключение css
 import './css/styles.css';
 
-const container = document.querySelector('.container');
-container.classList.add('container');
 
-const h1 = document.createElement('h1');
-h1.innerText = messages.h1;
-
-container.appendChild(h1);
