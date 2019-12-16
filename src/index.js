7b23d1e3bbe0d347e116650a98a7d2fe03a08480
Moskvichev Vladislav <moskvichev.exceedteam@gmail.com>
2019-12-16 19:28:48 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
-import { messages } from './messages';
+import {messages} from './messages';
 // Подключение css
 import './css/styles.css';
+
+const myList = document.querySelector('#list').onchange = () => {
+        let listValue = document.querySelector('#list');
+        let txt = document.querySelector('.text');
+        for (let i = 0; i < listValue.length; i++) {
+            if (listValue.options[i].selected == true) {
+                txt.value = listValue.options[i].text;
+            }
+        }
+    };
+
+const button = document.querySelector('#btn').onclick = () => {
+        let listValue = document.querySelector('#list');
+        let txt = document.querySelector('.text').value;
+        txt = txt.trim();
+        for (let i = 0; i < listValue.length; i++) {
+            if (listValue.options[i].selected == true && txt != '') {
+                listValue.options[i].text = txt;
+            }
+            else if (txt == ''){
+                document.querySelector('.text').classList.add('error');
+            }
+        }
+    };
+
+const deleteError = document.querySelector('.text').onfocus = () => {
+    let txt =  document.querySelector('.text');
+    txt.classList.remove('error');
+};
 
 
