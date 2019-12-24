df46b5db11137fc2430dd8d1bf62ca2f615a5c2c
Moskvichev Vladislav <moskvichev.exceedteam@gmail.com>
2019-12-24 20:53:00 +0300

    fixed 2

--- src/index.js
+++ src/index.js
@@ -8,24 +8,25 @@ let txt = document.querySelector('.text'); // onfocus
 
 button.addEventListener("click", function () {
     txt.value = txt.value.trim();
-    if (txt.value) {
-        for (let i = 0; i < listValue.length; i++) {
-            if (listValue.options[i].selected) {
-                listValue.options[i].text = txt.value;
-            }
+    if (!txt.value) {
+        return txt.classList.add('error');
+    }
+    for (let i = 0; i < listValue.length; i++) {
+        if (listValue.options[i].selected) {
+            listValue.options[i].text = txt.value;
         }
-    } else {
-        txt.classList.add('error');
     }
 });
 
 listValue.addEventListener("change", function () {
+
+    if (txt.classList.contains('error')) {
+        txt.classList.remove('error');
+    }
+
     for (let i = 0; i < this.length; i++) {
         if (this.options[i].selected) {
             txt.value = this.options[i].text;
-            if (txt.classList.contains('error')) {
-                txt.classList.remove('error');
-            }
         }
     }
 });
@@ -33,4 +34,10 @@ listValue.addEventListener("change", function () {
 txt.addEventListener("focus", function () {
     this.classList.remove('error');
 });
+
+window.onload = function () {
+    txt.value = listValue.options[listValue.selectedIndex].text;
+}
+
+
 
