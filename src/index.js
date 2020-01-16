8fda9bd7f9539906df42d183c0157c6e5d72036f
Moskvichev Vladislav <moskvichev.exceedteam@gmail.com>
2020-01-16 22:15:26 +0300

    fixed 7

--- src/index.js
+++ src/index.js
@@ -7,21 +7,39 @@ let editButton = document.querySelector('#edit-btn'); // onclick
 let addButton = document.querySelector('#add-btn'); // onclick
 let txtEdit = document.querySelector('#text-edit'); // onfocus
 let txtAdd = document.querySelector('#text-add'); // onfocus
+let todos;
+let arr;
+
+function toLocal(){
+    todos = Array.from(listValue).map(value => value.label);
+    localStorage.setItem('todos', JSON.stringify(todos));
+    arr = localStorage.getItem('todos');
+    arr = JSON.parse(arr);
+
+}
+
+// if (localStorage.getItem('arr')) {
+//     for (let i = 0; i < arr.length; i++) {
+//         listValue.options[i].label = arr[i];
+//     }
+// }
+
 
 editButton.addEventListener("click", function () {
     txtEdit.value = txtEdit.value.trim();
 
-    const checkRepeat = Array.from(listValue).some(value => value.label == txtEdit.value);
-    if (checkRepeat) return txtEdit.classList.add('error');
+    const isRepeat = Array.from(listValue).some(value => value.label == txtEdit.value);
+    if (isRepeat) return txtEdit.classList.add('error');
 
     if (!txtEdit.value) {
         return txtEdit.classList.add('error');
     }
     for (let i = 0; i < listValue.length; i++) {
-        if (listValue.options[i].selected && !checkRepeat) {
+        if (listValue.options[i].selected && !isRepeat) {
             listValue.options[i].text = txtEdit.value;
         }
     }
+    toLocal();
 
 });
 
@@ -36,6 +54,7 @@ listValue.addEventListener("change", function () {
             txtEdit.value = this.options[i].text;
         }
     }
+    toLocal();
 
 });
 
@@ -60,10 +79,14 @@ addButton.addEventListener("click", function () {
     listValue.append(myOption);
 
     txtAdd.value = null;
+    toLocal();
 
 });
 
 txtAdd.addEventListener("focus", function () {
     this.classList.remove('error');
 });
+
+
+
 
